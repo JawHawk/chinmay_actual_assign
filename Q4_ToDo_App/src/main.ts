78c8d9f8f99fa061f2391d35fba41a695fc39f90
jawhawk <mavenal2143@gmail.com>
2024-09-21 15:38:17 +0530

    feat: Initialized repo and To Do App

---
+++ Q4_ToDo_App/src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <p class="read-the-docs">
+      This is a sample To Do List Application !
+    </p>
+  </div>
+`
